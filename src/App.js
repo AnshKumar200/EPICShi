f661359a44bdb7191718131e4885c4dd23352ea1
Ansh Kumar <124369727+AnshKumar200@users.noreply.github.com>
2024-12-11 11:58:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Card from './components/Card';
 import Header from './components/Header';
-import Footer from '/components/Footer';
+import Footer from './components/Footer';
 
 function App() {
     return (
